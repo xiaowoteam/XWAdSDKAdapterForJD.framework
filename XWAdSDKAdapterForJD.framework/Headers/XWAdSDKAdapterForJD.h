3751b5962654b098e874b886e03515867fece058
laole918 <laole918@qq.com>
2021-10-14 18:34:14 +0800

    first commit

---
+++ XWAdSDKAdapterForJD.framework/Headers/XWAdSDKAdapterForJD.h
@@ -0,0 +1,18 @@
+//
+//  XWAdSDKAdapterForJD.h
+//  XWAdSDKAdapterForJD
+//
+//  Created by laole918 on 2021/9/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XWAdSDKAdapterForJD.
+FOUNDATION_EXPORT double XWAdSDKAdapterForJDVersionNumber;
+
+//! Project version string for XWAdSDKAdapterForJD.
+FOUNDATION_EXPORT const unsigned char XWAdSDKAdapterForJDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XWAdSDKAdapterForJD/PublicHeader.h>
+
+
